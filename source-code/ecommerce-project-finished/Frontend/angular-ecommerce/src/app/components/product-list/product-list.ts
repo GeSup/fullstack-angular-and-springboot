166af7f2ed999eca54a3df960db486110d1bd569
Jerzy Supryn (Gesup) <jerzy.supryn.dev@gmail.com>
2026-03-15 13:13:45 +0100

    Section 12: eCommerce Project - Search for Products by Category

--- source-code/ecommerce-project-finished/Frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
+++ source-code/ecommerce-project-finished/Frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +11,7 @@ import { Product } from '../../common/product';
 })
 export class ProductList implements OnInit {
   private _products = signal<Product[]>([]);
+  curentCategoryId: number = 1;
 
   get products(): Product[] {
     return this._products();
@@ -19,14 +21,29 @@ export class ProductList implements OnInit {
     this._products.set(data);
   }
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   private listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.curentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not category id available ... default to category id 1
+      this.curentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.curentCategoryId).subscribe(
       data => {
         this.products = data;
       }
