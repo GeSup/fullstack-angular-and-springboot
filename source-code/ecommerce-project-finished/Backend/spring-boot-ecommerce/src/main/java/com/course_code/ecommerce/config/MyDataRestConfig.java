daf9446ddfc2ab42aa21f678a435c7891c65dbea
Jerzy Supryn (GeSup) <jerzy.supryn.dev@gmail.com>
2026-07-17 16:28:18 +0200

    Section 20: eCommerce Project - Checkout Form - Drop-Down Lists

--- source-code/ecommerce-project-finished/Backend/spring-boot-ecommerce/src/main/java/com/course_code/ecommerce/config/MyDataRestConfig.java
+++ source-code/ecommerce-project-finished/Backend/spring-boot-ecommerce/src/main/java/com/course_code/ecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.course_code.ecommerce.config;
 
+import com.course_code.ecommerce.entity.Country;
 import com.course_code.ecommerce.entity.Product;
 import com.course_code.ecommerce.entity.ProductCategory;
+import com.course_code.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.jspecify.annotations.NonNull;
@@ -31,21 +33,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        // disable HTTP methods: PUT, POST, DELETE and PATCH
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-        
         // call an internal helper method
         exposeIds(config);
 
+    }
+
+    private static void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((_, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
--- source-code/ecommerce-project-finished/Frontend/angular-ecommerce/src/app/components/checkout/checkout.ts
+++ source-code/ecommerce-project-finished/Frontend/angular-ecommerce/src/app/components/checkout/checkout.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShopFormService } from '../../services/shop-form-service';
+import { Country } from '../../common/country';
+import { State } from '../../common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +16,17 @@ export class Checkout implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {}
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService,
+  ) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -45,11 +58,35 @@ export class Checkout implements OnInit {
         expirationYear: [''],
       }),
     });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('startMonth: ' + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe((data) => {
+      console.log('Retrieved credit card months: ' + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    // populate credit card years
+    this.shopFormService.getCreditCardYears().subscribe((data) => {
+      console.log('Retrieved credit card years: ' + JSON.stringify(data));
+      this.creditCardYears = data;
+    });
+
+    // populate countries
+    this.shopFormService.getCountries().subscribe((data) => {
+      console.log('Retrieved countries: ' + JSON.stringify(data));
+      this.countries = data;
+    });
   }
 
   onSubmit() {
     console.log('Handling the submit button');
-    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log('Customer: ', this.checkoutFormGroup.get('customer')?.value);
+    console.log('Shipping Address: ', this.checkoutFormGroup.get('shippingAddress')?.value);
+    console.log('Billing Address: ', this.checkoutFormGroup.get('billingAddress')?.value);
+    console.log('Credit Card: ', this.checkoutFormGroup.get('creditCard')?.value);
   }
 
   copyShippingAddressToBillingAddress(event: any) {
@@ -57,8 +94,56 @@ export class Checkout implements OnInit {
       this.checkoutFormGroup.controls['billingAddress'].setValue(
         this.checkoutFormGroup.controls['shippingAddress'].value,
       );
+
+      // bug fix for states
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      // bug fix for states
+      this.billingAddressStates = [];
     }
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+    const startMonth: number = currentYear === selectedYear ? new Date().getMonth() + 1 : 1;
+
+    // if (currentYear === selectedYear) {
+    //   startMonth = new Date().getMonth() + 1;
+    // } else {
+    //   startMonth = 1;
+    // }
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe((data) => {
+      console.log('Retrieved credit card months: ' + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.shopFormService.getStates(countryCode).subscribe((data) => {
+      if (formGroupName === 'shippingAddress') {
+        this.shippingAddressStates = data;
+      } else {
+        this.billingAddressStates = data;
+      }
+
+      // select first item by default
+      formGroup?.get('state')?.setValue(data[0]);
+    });
   }
 }
