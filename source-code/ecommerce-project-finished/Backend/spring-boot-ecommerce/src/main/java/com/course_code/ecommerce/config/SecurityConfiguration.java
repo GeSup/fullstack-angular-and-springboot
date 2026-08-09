c6654d7fe419b74f0b356cc6f6e46d5146e66213
Jerzy Supryn (GeSup) <jerzy.supryn.dev@gmail.com>
2026-08-09 13:11:43 +0200

    Section 30: Release 3.0 - Security - Order History - Secure Backend

---
+++ source-code/ecommerce-project-finished/Backend/spring-boot-ecommerce/src/main/java/com/course_code/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.course_code.ecommerce.config;
+
+import com.auth0.spring.boot.Auth0AuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, Auth0AuthenticationFilter authFilter) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(crsf -> crsf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/orders/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                        ))
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
