f4972de37413e6edcde73d9d0c60bba313ae0258
Jerzy Supryn (GeSup) <jerzy.supryn.dev@gmail.com>
2026-08-11 17:29:02 +0200

    refactor security

--- source-code/ecommerce-project-finished/Backend/spring-boot-ecommerce/src/main/java/com/course_code/ecommerce/config/SecurityConfiguration.java
+++ source-code/ecommerce-project-finished/Backend/spring-boot-ecommerce/src/main/java/com/course_code/ecommerce/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +18,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, Auth0AuthenticationFilter authFilter) throws Exception {
         return http
                 .cors(Customizer.withDefaults())
-                .csrf(crsf -> crsf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/orders/**").authenticated()
